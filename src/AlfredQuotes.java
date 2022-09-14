35fb4114b3e63040c6553c57c76d98392b5a56a7
tmax818 <tylermaxwell661@gmail.com>
2022-09-14 06:21:21 -0700

    init commit

---
+++ src/AlfredQuotes.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+public class AlfredQuotes {
+
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+
+    /**
+     * Returns a greeting that includes the person's name.
+     * @param name
+     * @return
+     */
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+        return "place holder for guest greeting return string";
+    }
+
+    /**
+     * Returns a polite announcement of the current date and time.
+     * @return
+     */
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        return "place holder for date announcement return string";
+    }
+
+    /**
+     * AlfredBot will periodically process audio (Alfred is listening.) Write a method that accepts any string of conversation and responds appropriately.
+     * @param conversation
+     * @return
+     */
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        return "for snarky response return string";
+    }
+
+    // NINJA BONUS
+    // See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
